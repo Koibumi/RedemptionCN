c131fee791e0a96776630834366e7ee086644357
madamamada <1194826882@qq.com>
2024-02-14 01:05:19 +0800

    晕

--- Patch/ChatMessages.cs
+++ Patch/ChatMessages.cs
@@ -5,34 +5,17 @@ namespace RedemptionCN.Patch
 {
     public class ChatMessages : PatchBase
     {
-        public override void Load()
+        /*public override void Load()
         {
             Terraria.On_Main.NewText_string_byte_byte_byte += NewText_string_byte_byte_byte;
-            Terraria.On_Main.NewTextMultiline += NewTextMultiline;
         }
         private static void NewText_string_byte_byte_byte(Terraria.On_Main.orig_NewText_string_byte_byte_byte orig, string newText, byte R = byte.MaxValue, byte G = byte.MaxValue, byte B = byte.MaxValue)
         {
-            if (newText == null)
-            else orig
-            {
-                return;
-            }
             if (newText == "TARGET UNIMPORTANT...")
             {
                 orig.Invoke($"目标不重要......", R, G, B);
             }
-        }
-        private static void NewTextMultiline(Terraria.On_Main.orig_NewTextMultiline orig, string text, bool force = false, Color c = default, int WidthLimit = -1)
-        {
-            if (text == null)
-            {
-                return;
-            }
-            if (text == "TARGET UNIMPORTANT...")
-            {
-                orig.Invoke($"目标不重要......", force, c, WidthLimit);
-            }
-        }
+        }*/
     }
 }
                 
